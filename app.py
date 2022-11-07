3cbb189b0ada94ef646a87ebe34479fe3547177d
jwitherell24 <witherellj24@gmail.com>
2022-11-06 20:27:49 -0500

    Finished routes and began template files

--- app.py
+++ app.py
@@ -22,6 +22,33 @@ def create_project():
     return render_template("createproject.html")
 
 
+@app.route("/projects/<id>")
+def project(id):
+    project = Project.query.get_or_404(id)
+    return render_template("project.html", project=project)
+
+
+@app.route("/projects/<id>/edit", methods=["GET", "POST"])
+def edit_project(id):
+    project = Project.query.get_or_404(id)
+    if request.form:
+        project.title = request.form["title"]
+        project.date_finished = request.form["date finished"]
+        project.skills_used = request.form["skills used"]
+        project.description = request.form["description"]
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("editproject.html", project=project)
+
+
+@app.route("/projects/<id>/delete")
+def delete_project(id):
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
